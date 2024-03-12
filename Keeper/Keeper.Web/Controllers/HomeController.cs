7efe9c9eb0ce805c2bfb4727aeaafaacd6362816
sneg <tsvetkovasnezhana555@yandex.ru>
2024-03-12 17:22:55 +0300

    create Keeper.Web

---
+++ Keeper/Keeper.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Keeper.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Authorization()
+        {
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Keeper/Keeper.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Keeper.Web.MvcApplication" Language="C#" %>
