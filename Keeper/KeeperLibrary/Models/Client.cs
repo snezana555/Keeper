0dbf9136926662b73edf3fba02acdcf774a43035
sneg <tsvetkovasnezhana555@yandex.ru>
2024-03-12 14:21:36 +0300

    update KeeperLibrary

--- Keeper/KeeperLibrary/Models/Client.cs
+++ Keeper/KeeperLibrary/Models/Client.cs
@@ -6,7 +6,7 @@ namespace KeeperLibrary.Models
 {
     public class Client
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
---
+++ Keeper/KeeperLibrary/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeeperLibrary.Models
+{
+    public class Department
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }    
+        public List<Employee>? Employees { get; set; }
+    }
+}
