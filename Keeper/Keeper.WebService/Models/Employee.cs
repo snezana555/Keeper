e4900c51849434caf5c81e3d2013d688aa57435f
sneg <tsvetkovasnezhana555@yandex.ru>
2024-03-12 18:27:59 +0300

    create Keeper.WebService

---
+++ Keeper/Keeper.WebService/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeeperLibrary.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public Department? Department { get; set; }
+        public string? Section { get; set; }
+
+    }
+}
---
+++ Keeper/Keeper.WebService/Models/Visitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeeperLibrary.Models
+{
+    public class Visitor
+    {
+        public Guid Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string? Patronymic { get; set; }
+        public string? Phone { get; set; }
+        public string Email { get; set; }
+        public string Remark { get; set; }
+        public string? Company { get; set; }
+        public DateTime DateBoth { get; set; }
+        public PassportData PassportData { get; set; }
+        public string? Image { get; set; }
+        public List<Request> Requests { get; set; } 
+
+    }
+}
