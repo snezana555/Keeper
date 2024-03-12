c88c2b38b686ba89d01b14c1e5cefb63e0be4527
sneg <tsvetkovasnezhana555@yandex.ru>
2024-03-12 14:21:36 +0300

    update KeeperLibrary

---
+++ Keeper/KeeperLibrary/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeeperLibrary.Models
+{
+    public class Department
+    {
+        public int Guid { get; set; }
+        public string Name { get; set; }    
+        public List<Employee>? Employees { get; set; }
+    }
+}
--- Keeper/KeeperLibrary/Models/Visitor.cs
+++ Keeper/KeeperLibrary/Models/Visitor.cs
@@ -6,7 +6,7 @@ namespace KeeperLibrary.Models
 {
     public class Visitor
     {
-        public int Id { get; set; }
+        public int Guid { get; set; }
         public string Surname { get; set; }
         public string Name { get; set; }
         public string? Patronymic { get; set; }
@@ -15,9 +15,9 @@ namespace KeeperLibrary.Models
         public string Remark { get; set; }
         public string? Company { get; set; }
         public DateTime DateBoth { get; set; }
-        public int Number { get; set; }
-        public int Series { get; set; }
-        public byte[]? Image { get; set; }
+        public PassportData PassportData { get; set; }
+        public string? Image { get; set; }
+        public List<Request> Requests { get; set; } 
 
     }
 }
