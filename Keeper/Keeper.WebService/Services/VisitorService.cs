aeb38a9a86fc104c5584ccf0cc16c82e0f5ac0a1
sneg <tsvetkovasnezhana555@yandex.ru>
2024-03-14 01:21:59 +0300

    add Validators and Services

---
+++ Keeper/Keeper.WebService/Services/VisitorService.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Core;
+using Keeper.Data;
+using KeeperLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Keeper.WebService.Services
+{
+    public class VisitorService : IVisitorService
+    {
+        private readonly KeeperDbContext _db;
+
+        public VisitorService(KeeperDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Visitor> Add(string surname,
+                                    string patronymic,
+                                    string phone,
+                                    string email,
+                                    string remark,
+                                    string company,
+                                    DateTime dateBoth,
+                                    PassportData PassportData,
+                                    string image)
+        {
+            //TO DO: добавление валидации//
+
+            Visitor visitor = new Visitor();
+            visitor.Surname = surname;
+            visitor.Patronymic = patronymic;
+            visitor.Phone = phone;
+            visitor.Email = email;
+            visitor.Remark = remark;
+            visitor.Company = company;
+            visitor.DateBoth = dateBoth;
+            visitor.PassportData = PassportData;
+            visitor.Image = image;
+            _db.Visitors.Add(visitor);
+            await _db.SaveChangesAsync();
+            return visitor;
+        }
+
+        public async Task Remove(int id)
+        {
+            var visitor = _db.Visitors.FindAsync(id);
+            _db.Remove(visitor);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Edit(Visitor visitor)
+        {
+            _db.Entry(visitor).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ Keeper/Keeper.WebService/Validators/RequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using KeeperLibrary.Models;
+using FluentValidation;
+namespace Keeper.WebService.Validators
+{
+    public class RequestValidator : AbstractValidator<Request>
+    {
+        public RequestValidator() 
+        { 
+            // TODO:  создание валидации //
+        }
+    }
+}
