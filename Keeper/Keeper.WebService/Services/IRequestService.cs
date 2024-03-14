ffeddc5eeb65f1b34bab2a4d50cda0b631e2df52
sneg <tsvetkovasnezhana555@yandex.ru>
2024-03-14 16:38:20 +0300

    update Services

--- Keeper/Keeper.WebService/Services/IRequestService.cs
+++ Keeper/Keeper.WebService/Services/IRequestService.cs
@@ -6,7 +6,7 @@ namespace Keeper.WebService.Services
     public interface IRequestService
     {
         Task Add(RequestCreationDto dto);
-        Task Remove(int id);
+        Task Remove(int id); 
         Task Edit(Request request);
         Task ChangeStatus(int id);
     }
